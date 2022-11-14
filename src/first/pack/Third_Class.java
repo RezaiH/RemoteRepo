549c949973e8a4749c9706a5935a05c61563937c
Hussain Rezai <rezaih637@gmail.com>
2022-11-13 21:27:51 -0500

    First commit

--- src/first/pack/Third_Class.java
+++ src/first/pack/Third_Class.java
@@ -2,4 +2,9 @@ package first.pack;
 
 public class Third_Class {
 
+	public static void showChange() {
+		String name = "Hussain";
+		String lName = "Rezai";
+		System.out.println(name + " "+ lName + " is from Afghanistan!");
+	}
 }
